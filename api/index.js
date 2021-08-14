d570858602aaf2688aa4644f0892490cdfec8f99
adutta <atulrajdutta@gmail.com>
2021-08-14 17:39:48 +0530

    Removed file that shouldn't be tracked

--- api/index.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const controller = require('./api.controller');
-
-router.get('/responses',controller.domainResponses);
-router.post('/responses/save',controller.domainResponsesSave);
-module.exports = router;
