cc3d8cc75171b24df3472a0217861c06de056261
adutta <atulrajdutta@gmail.com>
2021-08-14 16:48:42 +0530

    initial commit

---
+++ api/api.controller.js
@@ -0,0 +1,39 @@
+const request = require('request-promise');
+const fs = require('fs');
+const yaml = require('js-yaml');
+
+module.exports.domainResponses = async(req,res)=>{
+    try {
+        let fileContents = fs.readFileSync('/home/adutta/rasa_client/domain.yml', 'utf8');
+        let data = yaml.load(fileContents);
+    
+        console.log(data['responses']["utter_ask_civil_number"][0]['text']);
+        res.send({success:true,data:data['responses']})
+    } catch (error) {
+        console.log(error);
+        res.status(400).send({success:false,error:error})
+
+    }
+}
+
+module.exports.domainResponsesSave = async(req,res)=>{
+    try {
+        let fileContents = fs.readFileSync('/home/adutta/rasa_client/domain.yml', 'utf8');
+        let data = yaml.load(fileContents);
+        let responseName = req.body.responseName;   
+        console.log(responseName);
+        data['responses'][responseName][0]['text'] = req.body.newResponse;
+    
+        fs.writeFile('/home/adutta/rasa_client/domain.yml', yaml.dump(data), (err) => {
+            if (err) {
+                console.log("Can not write to the file")
+                console.log(err);
+            }
+        });
+        res.send({success:true,data:data['responses']})
+    } catch (error) {
+        console.log(error);
+        res.status(400).send({success:false,error:error})
+
+    }
+}
