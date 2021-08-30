be5087051d72a656c215108524061fe2cf15a006
atulrajdutta <atulrajdutta@gmail.com>
2021-08-30 12:52:35 +0530

    added select model api

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const controller = require('./api.controller');
 router.get('/responses',controller.domainResponses);
 router.post('/responses/save',controller.domainResponsesSave);
 router.get('/model/train',controller.trainModel);
-router.get('/model',controller.useTrainedModel);
+router.post('/model',controller.useTrainedModel);
 router.post('/faqData',controller.faqIntent);
 router.post('/faqData/save',controller.faqSave);
 // router.post('/model',controller.useTrainedModel);
